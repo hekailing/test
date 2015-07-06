b29800a87deb87403c26ce8438335aba3b85abb6
hekailing <1034716312@qq.com>
2015-07-06 15:52:23 +0800

    kkk

--- test.cpp
+++ test.cpp
@@ -6,6 +6,6 @@ using namespace std;
 int main() {
   cout << "This is the origin file" << endl;
   cout << "Add a line from feature/test" << endl;
-  cout << "Add a line first" << endl;
+  cout << "Add a line from feature/test2" << endl;
   return 0;
 }
