744cc310cab90ba4ee3e991e1df5cc1c7a81a994
hekailing <1034716312@qq.com>
2015-07-06 15:43:35 +0800

    add a line secondly

---
+++ test.cpp
@@ -0,0 +1,11 @@
+#include  <unistd.h>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  cout << "This is the origin file" << endl;
+  cout << "Add a line from feature/test" << endl;
+  cout << "Add a line secondly" << endl;
+  return 0;
+}
