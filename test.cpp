9d5467b191c20040f693b0871e2723712bf3816a
hekailing <1034716312@qq.com>
2015-07-06 14:22:02 +0800

    add test first

--- test.cpp
+++ test.cpp
@@ -6,5 +6,6 @@ using namespace std;
 int main() {
   cout << "This is the origin file" << endl;
   cout << "Add a line from feature/test" << endl;
+  cout << "Add a line from feature/test first" << endl;
   return 0;
 }
